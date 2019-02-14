375ccc44a8c4a754ff10711e91ef5685eabe72e3
Sam Bell <sam.bell@accenture.com>
2019-02-14 11:30:39 +0000

    Updates

---
+++ precache-manifest.82308e437a21b8c87a23e83352a8ba37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "49dfb41f097f54fcfbd932c95d32b641",
+    "url": "/myportfolio/static/media/pixelcitypink.49dfb41f.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myportfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6b6f225bb9dac2e07552",
+    "url": "/myportfolio/static/js/runtime~main.6b6f225b.js"
+  },
+  {
+    "revision": "c60dd8e3f5257d8f6ac3",
+    "url": "/myportfolio/static/js/main.c60dd8e3.chunk.js"
+  },
+  {
+    "revision": "d7b100a7500de7b1db82",
+    "url": "/myportfolio/static/js/1.d7b100a7.chunk.js"
+  },
+  {
+    "revision": "c60dd8e3f5257d8f6ac3",
+    "url": "/myportfolio/static/css/main.f3b052da.chunk.css"
+  },
+  {
+    "revision": "d7b100a7500de7b1db82",
+    "url": "/myportfolio/static/css/1.8588c9e8.chunk.css"
+  },
+  {
+    "revision": "9c1a0e57a8f84fe54bc109398d4443f0",
+    "url": "/myportfolio/index.html"
+  }
+];
