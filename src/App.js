e0ac9e1c94fd399ffb795f35a437d08d5f401271
Sam Bell <sam.bell@accenture.com>
2019-02-26 15:00:52 +0000

    added google analytics

--- src/App.js
+++ src/App.js
@@ -12,6 +12,12 @@ import {
   NavLink
 } from "reactstrap";
 import Landing from "./components/Landing";
+import ReactGA from 'react-ga';
+
+function initializeReactGA() {
+  ReactGA.initialize('UA-108148491-2');
+  ReactGA.pageview('/homepage');
+}
 
 class App extends Component {
   render() {
